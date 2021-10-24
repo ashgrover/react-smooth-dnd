4b73dc2ef806aa09c6e28251566466fcc398ce87
Ash G <ashgrover@gmail.com>
2021-10-24 01:42:41 -0600

    Add support for non-draggable items.

--- src/Draggable.tsx
+++ src/Draggable.tsx
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { constants } from 'smooth-dnd-modified';
-const {
-	wrapperClass
-} = constants;
+const { wrapperClass, nonDraggableClass } = constants;
 
 export interface DraggableProps {
 	render?: () => React.ReactElement;
 	className?: string;
+	canDrag?: boolean;
 }
 
 class Draggable extends Component<DraggableProps> {
 	public static propsTypes = {
 		render: PropTypes.func,
 		className: PropTypes.string,
+		canDrag: PropTypes.bool
 	}
 
 	render() {
+		const dragItemClass = (this.props.canDrag == null || this.props.canDrag) ? wrapperClass : nonDraggableClass;
+
 		if (this.props.render) {
-			return React.cloneElement(this.props.render(), { className: wrapperClass });
+			return React.cloneElement(this.props.render(), { className: dragItemClass });
 		}
-		
-		const clsName = `${this.props.className ? (this.props.className + ' ') : ''}`
+
+		const clsName = `${this.props.className ? (this.props.className + ' ') : ''}${dragItemClass}`;
 		return (
-			<div {...this.props} className={`${clsName}${wrapperClass}`} >
+			<div className={clsName}>
 				{this.props.children}
 			</div>
 		);
